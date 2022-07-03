adb7d76ead3be8c414688d024a5f737f6bfffc81
Flesh-vadim22 <vadyusha.shubin.2001@mail.ru>
2022-07-03 17:11:58 +0300

    initional commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='participation in the Yaroslavl region championship',
+    author='Shubin_Vadim',
+    license='MIT',
+)
